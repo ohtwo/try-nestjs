0203300209beadd1710b17f5be902f7e51fc78b8
ohtwo <hello@tinygram.dev>
2024-01-25 08:13:17 +0900

    feat: ch6.3 - dynamic module / config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService, ServiceA, ServiceB } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [UsersModule, EmailModule, ConfigModule.forRoot({
+    envFilePath: (process.env.NODE_ENV === 'production') ? '.prod.env'
+    : (process.env.NODE_ENV === 'stage') ? '.stage.env' : '.dev.env'
+  })],
   controllers: [AppController],
   providers: [AppService, ServiceA, ServiceB],
 })
