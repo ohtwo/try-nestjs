e17a097f6ed5ced7884e1e25388fe449dd77e8b4
ohtwo <hello@tinygram.dev>
2024-02-21 08:43:28 +0900

    feat: ch7.4.2 - class-validator, class-transformer

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,27 @@
-import { IsString, MinLength, IsEmail, MaxLength } from "class-validator";
+import { BadRequestException } from "@nestjs/common";
+import { Transform } from "class-transformer";
+import { IsString, MinLength, IsEmail, MaxLength, Matches } from "class-validator";
 
 export class CreateUserDto {
+  @Transform(params => {
+    console.log(params);
+    const { value, obj } = params;
+    if (obj.password.includes(obj.name.trim())) {
+      throw new BadRequestException('password cannot contain the same string as name.')
+    }
+    return value.trim();
+  })
   @IsString()
-  @MinLength(1)
-  @MaxLength(20)
+  @MinLength(2)
+  @MaxLength(30)
   readonly name: string;
 
+  @IsString()
   @IsEmail()
+  @MaxLength(60)
   readonly email: string;
+
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
   readonly password: string;
 }
